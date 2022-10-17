5f1e19be307ef1aac40c63e5b1648e5717f18036
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-10-16 21:35:05 -0700

    kvp's and dictionaries as arguments

--- variationofargs.py
+++ variationofargs.py
@@ -1,4 +1,4 @@
 def save_user(**user): #key value pairs, dictionary
- print(user)
+ print(user["name"])
 
-save_user(id=1, name='john', age=22) #key value pairs, dictionary
+save_user(id=1, name='john', age=22) #key value pairs, dictionary, python auto packages to dictionary
