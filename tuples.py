64842b100565a4109a9088e77d7d494ea46ab141
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-10-16 21:34:16 -0700

    pass multiple kvps to function

---
+++ tuples.py
@@ -0,0 +1,5 @@
+def variable_num_args(*numbers):
+ print(numbers)
+ 
+
+variable_num_args(3,4,5,6,8) 
---
+++ variationofargs.py
@@ -0,0 +1,4 @@
+def save_user(**user): #key value pairs, dictionary
+ print(user)
+
+save_user(id=1, name='john', age=22) #key value pairs, dictionary
