ed75c7999fefe5c6342708a2b1df7638dcf8047c
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-10-16 21:25:55 -0700

    optional params

--- increment.py
+++ increment.py
@@ -1,4 +1,6 @@
-def increment(number, by):
+def increment(number, by=1):
  return number + by
 
-print(increment(2,1))
+print(increment(2))
+
+# calling defaults with equal in the original definition
