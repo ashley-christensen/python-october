1b7a0a30ae39eb8de23e6e51a61117d45ebbe417
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-10-16 21:31:06 -0700

    tuples loops

--- variableargs.py
+++ variableargs.py
@@ -1,10 +1,13 @@
 def multiply(*numbers):
+ total = 1
  for num in numbers:
   print("num: " + str(num))
+  total *= num
+ return total 
 
 
-multiply(2,3,4,4,5)
-
+result = multiply(2,3,4,4,5)
+print(result)
 
 
 # tuples = collection of objects CANNOT MODIFY!
