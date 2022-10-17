c9f8dea9c397168908261ba3277969b77aeeca41
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-10-16 21:29:25 -0700

    tuples

--- increment.py
+++ increment.py
@@ -1,6 +1,6 @@
-def increment(number, by=1):
- return number + by
+def increment(number, another, by=1):
+ return number + another + by
 
-print(increment(2))
+print(increment(2, 3, 5))
 
 # calling defaults with equal in the original definition
---
+++ variableargs.py
@@ -0,0 +1,11 @@
+def multiply(*numbers):
+ for num in numbers:
+  print("num: " + str(num))
+
+
+multiply(2,3,4,4,5)
+
+
+
+# tuples = collection of objects CANNOT MODIFY!
+# iterable
